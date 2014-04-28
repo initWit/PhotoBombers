d61db350353cf61b650d39be628889ebd964dadb
Robert Figueras <robertfigueras@Roberts-iMac-4.local>
2014-04-28 09:18:10 -0500

    Initial Commit

---
+++ Photo Bombers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Photo Bombers
+//
+//  Created by Robert Figueras on 4/28/14.
+//  Copyright (c) 2014 AppSpaceship. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
